078d5675173c06dd0dcd34b8dff7932276cb0ef1
alek <wait82985@gmail.com>
2023-06-13 18:27:47 +0600

    initial commit. 2 test cases

---
+++ src/main/java/Solution.java
@@ -0,0 +1,6 @@
+public class Solution {
+    public int numSquares(int n) {
+        //TODO
+        return 0;
+    }
+}
---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,18 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void numSquaresTest1() {
+        int n = 12;
+        int output = 3;
+        Assert.assertEquals(output, new Solution().numSquares(n));
+    }
+
+    @Test
+    public void numSquaresTest2() {
+        int n = 12;
+        int output = 3;
+        Assert.assertEquals(output, new Solution().numSquares(n));
+    }
+}
